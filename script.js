706a42727020f6e0bd069d7af8f8bc93b9db76aa
Randeepa123 <158833313+Randeepa123@users.noreply.github.com>
2024-05-05 21:58:06 +0530

    End of  the admin page

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function toggleAside() {
-    var sidebar = document.querySelector('.sidebar');
-    var menuToggle = document.querySelector('.menu-toggle');
-    
-    sidebar.classList.toggle('open');
-    menuToggle.classList.toggle('open');
-}
+function menuToggle() {
+    const toggleMenu = document.querySelector(".menu");
+    toggleMenu.classList.toggle("active");}
